dd6727bdb733a31eae3a704d7c64fb2973fc8be9
Carlos Sanchez <carlo.s16@hotmail.com>
2020-06-01 23:42:30 -0400

    fixed issue with indices

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,9 +7,11 @@ class RingBuffer:
     def append(self, item):
         if len(self.storage) < self.capacity:
             self.storage.append(item)
+            self.curr_pos += 1
         else:
             if self.curr_pos == self.capacity:
                 self.curr_pos = 0
+                self.storage[self.curr_pos] = item
 
     def get(self):
         return self.storage
